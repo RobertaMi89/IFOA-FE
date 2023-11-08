2daec349f3b3ed44ceabc1385a912990aada57f7
RobertaMi89 <roberta.miosi@gmail.com>
2023-11-08 16:58:15 +0100

    esDays2Week3

--- week03/D7/assets/js/es.js
+++ week03/D7/assets/js/es.js
@@ -122,13 +122,82 @@ revealFooterLink();
         La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo (5 righe e 4 colonne)
      */
 
-const generateTable = function () {};
+const generateTable = function () {
+  const container = document.getElementById("tableArea");
+  const table = document.createElement("table");
+  table.style.width = "100%";
+
+  const headers = ["Immagine", "Prodotto", "Quantità", "Prezzo"];
+  const headerRow = document.createElement("tr");
+
+  headers.forEach((headerText) => {
+    const th = document.createElement("th");
+    th.innerText = headerText;
+    headerRow.appendChild(th);
+  });
+
+  table.appendChild(headerRow);
+
+  const images = [
+    "https://m.media-amazon.com/images/I/81yJf4K+L1L._AC_SY450_.jpg",
+    "https://m.media-amazon.com/images/I/61MRTh9xotL._AC_SX679_.jpg",
+    "https://www.tradeinn.com/f/13805/138054505/samsung-smartphone-galaxy-s21-5g-8gb-128gb-6.2.jpg",
+    "https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1661333885.70133010!500x500.png",
+    "https://tomatosmartphone.it/wp-content/uploads/2022/06/EU-IN_Green.png",
+  ];
+
+  images.forEach((imageUrl) => {
+    const newRow = document.createElement("tr");
+
+    const tdImage = document.createElement("td");
+    tdImage.innerHTML = `<img src="${imageUrl}" alt="smartphone" height="200" />`;
+
+    const tdProduct = document.createElement("td");
+    tdProduct.innerText = "Smartphone";
+
+    const tdQuantity = document.createElement("td");
+    tdQuantity.innerText = Math.floor(Math.random() * 50);
+
+    const tdPrice = document.createElement("td");
+    tdPrice.innerText = `${Math.floor(Math.random() * 500)}€`;
+
+    [tdImage, tdProduct, tdQuantity, tdPrice].forEach((td) => {
+      newRow.appendChild(td);
+    });
+
+    table.appendChild(newRow);
+  });
+
+  container.appendChild(table);
+};
+
+generateTable();
 
 /* ESERCIZIO 12
         Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
      */
 
-const addRow = function () {};
+
+  const addRow = function (imgUrl, productName, quantity, price) {
+    const table = document.querySelector("table")
+
+    const newRow = document.createElement("tr")
+
+    const tdImage = document.createElement("td")
+    tdImage.innerHTML = `<img src=${imgUrl} alt="smartphone" height="200" />`
+    const tdProduct = document.createElement("td")
+    tdProduct.innerText = productName
+    const tdQuantity = document.createElement("td")
+    tdQuantity.innerText = quantity
+    const tdPrice = document.createElement("td")
+    tdPrice.innerText = price + "€"
+
+    newRow.appendChild(tdImage)
+    newRow.appendChild(tdProduct)
+    newRow.appendChild(tdQuantity)
+    newRow.appendChild(tdPrice)
+    table.appendChild(newRow)
+};
 
 /* ESERCIZIO 14
        Crea una funzione che nasconda le immagini della tabella quando eseguita (usa button per far sparire le immagini)
