141a7c2c272f89699394da41f40798605c8f728b
RobertaMi89 <roberta.miosi@gmail.com>
2023-11-28 00:09:31 +0100

    week5 final project

--- WeekFinalProjects/Week 05/S5-P-Medium-Clone-Start/assets/js/script.js
+++ WeekFinalProjects/Week 05/S5-P-Medium-Clone-Start/assets/js/script.js
@@ -1,9 +1,20 @@
 let scrollpos = window.scrollY;
 const header = document.querySelector("header");
 const header_height = header.offsetHeight;
+const button = document.getElementById("getStartedBtn");
 
-const add_class_on_scroll = () => header.classList.add("whiteNav");
-const remove_class_on_scroll = () => header.classList.remove("whiteNav");
+const add_class_on_scroll = () => {
+  button.classList.remove("revertGreenButton");
+  header.classList.remove("revertWhiteNav");
+  button.classList.add("greenButton");
+  header.classList.add("whiteNav");
+};
+const remove_class_on_scroll = () => {
+  button.classList.remove("greenButton");
+  header.classList.remove("whiteNav");
+  button.classList.add("revertGreenButton");
+  header.classList.add("revertWhiteNav");
+};
 
 window.addEventListener("scroll", function () {
   scrollpos = window.scrollY;
@@ -13,6 +24,4 @@ window.addEventListener("scroll", function () {
   } else {
     remove_class_on_scroll();
   }
-
-  console.log(scrollpos);
 });
