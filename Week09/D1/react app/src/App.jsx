f7a098aa0d8de511ead5d9cbea31cbda6ab5392a
RobertaMi89 <roberta.miosi@gmail.com>
2024-01-08 17:36:59 +0100

    edit btn

--- Week09/D1/react app/src/App.jsx
+++ Week09/D1/react app/src/App.jsx
@@ -21,13 +21,8 @@ function App() {
       <h1>Esercizio IFOA</h1>
       <div className="card">
         <ButtonComponent text="Cliccami" btn={()=>{SetShowImage(!showImage)}}/>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
