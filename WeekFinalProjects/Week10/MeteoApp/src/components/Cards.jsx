49f89de39a6c57ef6215f653a7f960dde6e10d00
RobertaMi89 <roberta.miosi@gmail.com>
2024-01-19 18:11:27 +0100

    edit pages

---
+++ WeekFinalProjects/Week10/MeteoApp/src/components/Cards.jsx
@@ -0,0 +1,23 @@
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+const Cards = () => {
+  return (
+    <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 4 }).map((_, idx) => (
+        <Col key={idx}>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>City</Card.Title>
+              <Card.Text>inserire qui gradi, vento, umidità</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default Cards;
---
+++ WeekFinalProjects/Week10/MeteoApp/src/components/CustomFooter.jsx
@@ -0,0 +1,11 @@
+import Card from "react-bootstrap/Card";
+const CustomFooter = () => {
+  return (
+    <>
+      <Card>
+        <Card.Body>inserire qui description (clouds)</Card.Body>
+      </Card>
+    </>
+  );
+};
+export default CustomFooter;
